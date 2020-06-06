0519b38ca118fc10ee225316a62de9ab8c100040
Anukrit Tiwari <51081778+anukrit01@users.noreply.github.com>
2020-06-06 18:56:11 +0530

    Add files via upload

---
+++ first_program.cpp
@@ -0,0 +1,16 @@
+
+// Simple C++ program to display "Hello World" 
+  
+// Header file
+#include<iostream>  
+  
+using namespace std; 
+  
+// main function
+int main() 
+{ 
+    // prints hello world 
+    cout<<"Hello World"; 
+      
+    return 0; 
+} 
